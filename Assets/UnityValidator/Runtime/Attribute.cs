dfa1e76cd9e37625a8898419817917f9c6b54b98
inoue makoto <inouemakoto@team-lab.com>
2025-02-12 23:12:42 +0900

    Change attribute target

--- Assets/UnityValidator/Runtime/Attribute.cs
+++ Assets/UnityValidator/Runtime/Attribute.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace UnityValidator
 {
-    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Method)]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Method)]
     public class ValidateAttribute : PropertyAttribute
     {
         public bool IsRequired { get; private set; } = true;
